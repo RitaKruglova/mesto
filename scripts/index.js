3d1a044921a37a48e0653eb3d6c0f3ac8b19211d
Rita Kruglova <kruglova@404@mail.ru>
2023-01-26 13:21:47 +0300

    fix: scripts

--- scripts/index.js
+++ scripts/index.js
@@ -9,7 +9,7 @@ const aboutUser = container.querySelector('.profile__description');
 const resetButtons = document.querySelectorAll('.popup__reset-button');
 const addButton = container.querySelector('.profile__add-button');
 const popupAddCard = document.querySelector('.popup_type_add-card');
-const cards = container.querySelector('.cards');
+const cardsContainer = container.querySelector('.cards');
 const cardTemplate = document.querySelector('#card-template').content;
 const picturePopup = document.querySelector('.popup_type_picture');
 const popupImage = document.querySelector('.popup__image');
@@ -18,140 +18,88 @@ const addCardFormElement = document.querySelector('.popup__form_type_add-card');
 const pictureNameInput = document.querySelector('.popup__input_type_picture-name');
 const pictureLinkInput = document.querySelector('.popup__input_type_picture-link');
 const addCardButton = document.querySelector('.popup__submit-button_type_add-card');
+const recycleBinButtons = document.querySelectorAll('.card__recycle-bin');
 
 
-const initialCards = [
-  {
-    name: 'Киото',
-    link: 'https://images.unsplash.com/photo-1602764363500-e8e8455de955?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'
-  },
-  {
-    name: 'Окинава',
-    link: 'https://images.unsplash.com/photo-1625548894051-8ddd8650c6ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-  },
-  {
-    name: 'Аокигахара',
-    link: 'https://images.unsplash.com/photo-1579667246776-bb648259a001?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-  },
-  {
-    name: 'Осака',
-    link: 'https://images.unsplash.com/photo-1589451765662-547fb5445bb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
-  },
-  {
-    name: 'Фудзи',
-    link: 'https://images.unsplash.com/photo-1490806843957-31f4c9a91c65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
-  },
-  {
-    name: 'Токио',
-    link: 'https://images.unsplash.com/photo-1542051841857-5f90071e7989?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
-  }
-]; 
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function createCard(name, link) {
+  const card = cardTemplate.cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  cardImage.setAttribute('src', link);
+  cardImage.setAttribute('alt', name);
+  card.querySelector('.card__title').textContent = name;
+  card.querySelector('.card__recycle-bin').addEventListener('click', deleteCard);
+  card.querySelector('.card__like').addEventListener('click', like);
+  cardImage.addEventListener('click', () => openPicturePopup(name, link));
+  return card;
+}
 
 function addInitialCards(initialCards) {
   for (let i = 0; i < initialCards.length; i++) {
-    const cardImage = cardTemplate.querySelector('.card__image');
-    const cardTitle = cardTemplate.querySelector('.card__title');
-    cardImage.setAttribute('src', initialCards[i]['link']);
-    cardImage.setAttribute('alt', initialCards[i]['name']);
-    cardTitle.textContent = initialCards[i]['name'];
-    const card = cardTemplate.cloneNode(true);
-    cards.append(card);
+    const cardData = initialCards[i];
+    const card = createCard(cardData['name'], cardData['link']);
+    cardsContainer.append(card);
   }
 }
 
-addInitialCards(initialCards);
-
-const cardImages = container.querySelectorAll('.card__image');
-
-function openPicturePopup(event) {
-  picturePopup.classList.add('popup_opened');
-  popupImage.setAttribute('src', event.target.src);
-  popupImageName.textContent = event.target.parentElement.querySelector('.card__title').textContent;
+function openPicturePopup(name, link) {
+  openPopup(picturePopup);
+  popupImage.setAttribute('src', link);
+  popupImage.setAttribute('alt', name);
+  popupImageName.textContent = name;
 }
-
-cardImages.forEach(item => {
-  item.addEventListener('click', openPicturePopup);
-})
-
-
-
-
-const likeButtons = cards.querySelectorAll('.card__like');
 
 function like(event) {
   event.target.classList.toggle('card__like_active');
 }
 
-for (let index = 0; index < likeButtons.length; index++) {
-  const likeButton = likeButtons[index];
-  likeButton.addEventListener('click', like);
-  
-}
-
-
 function openEditProfilePopup() {
-  popupEditProfile.classList.add('popup_opened');
+  openPopup(popupEditProfile);
   usernameInput.value = username.textContent;
   aboutUserInput.value = aboutUser.textContent;
 }
 
-editButton.addEventListener('click', openEditProfilePopup);
-
-
-function openAddCardPopup() {
-  popupAddCard.classList.add('popup_opened');
-}
-
-addButton.addEventListener('click', openAddCardPopup);
-
-
-const recycleBinButtons = document.querySelectorAll('.card__recycle-bin');
-
-function deleteButton(event) {
+function deleteCard(event) {
   event.target.closest('.card').remove();
 }
 
-recycleBinButtons.forEach(item => {
-  item.addEventListener('click', deleteButton);
-})
-
-
 function addNewCard(event) {
   event.preventDefault();
-  const card = cardTemplate.cloneNode(true);
-  const cardImage = card.querySelector('.card__image');
-  cardImage.setAttribute('src', pictureLinkInput.value);
-  cardImage.setAttribute('alt', pictureNameInput.value);
-  card.querySelector('.card__title').textContent = pictureNameInput.value;
-  card.querySelector('.card__like').addEventListener('click', like);
-  cardImage.addEventListener('click', openPicturePopup);
-  card.querySelector('.card__recycle-bin').addEventListener('click', deleteButton);
-  cards.prepend(card);
-  popupAddCard.classList.remove('popup_opened');
-  pictureNameInput.value = '';
-  pictureLinkInput.value = '';
+  const card = createCard(pictureNameInput.value, pictureLinkInput.value);
+  cardsContainer.prepend(card);
+  closePopup(popupAddCard);
+  addCardFormElement.reset();
+  
 }
 
-addCardFormElement.addEventListener('submit', addNewCard);
-
-function handleFormSubmit(event) {
+function changeProfileInfo(event) {
   event.preventDefault();
   username.textContent = usernameInput.value;
   aboutUser.textContent = aboutUserInput.value;
-  popupEditProfile.classList.remove('popup_opened');
+  closePopup(popupEditProfile);
 }
 
-editFormElement.addEventListener('submit', handleFormSubmit);
-
 resetButtons.forEach(item => {
-  item.addEventListener('click', function() {
-    item.parentElement.parentElement.classList.remove('popup_opened');
-    document.querySelectorAll('.popup__input').forEach(item => {
-      item.value = '';
-    })
-  })
+  item.addEventListener('click', () => closePopup(item.closest('.popup')));
 });
 
+editButton.addEventListener('click', openEditProfilePopup);
 
+addCardFormElement.addEventListener('submit', addNewCard);
 
+addButton.addEventListener('click', () => openPopup(popupAddCard));
 
+recycleBinButtons.forEach(item => {
+  item.addEventListener('click', deleteCard);
+})
+
+editFormElement.addEventListener('submit', changeProfileInfo);
+
+addInitialCards(initialCards);
