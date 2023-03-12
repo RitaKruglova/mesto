023e30667e2d602e0312a418febb76ef33cd14a0
Rita Kruglova <kruglova@404@mail.ru>
2023-03-12 13:41:20 +0300

    refactor: change the file structure and paths

---
+++ scripts/utils/constants.js
@@ -0,0 +1,78 @@
+export const initialCards = [
+  {
+    name: 'Киото',
+    link: 'https://images.unsplash.com/photo-1602764363500-e8e8455de955?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80'
+  },
+  {
+    name: 'Окинава',
+    link: 'https://images.unsplash.com/photo-1625548894051-8ddd8650c6ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+  },
+  {
+    name: 'Аокигахара',
+    link: 'https://images.unsplash.com/photo-1579667246776-bb648259a001?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  },
+  {
+    name: 'Осака',
+    link: 'https://images.unsplash.com/photo-1589451765662-547fb5445bb8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80'
+  },
+  {
+    name: 'Фудзи',
+    link: 'https://images.unsplash.com/photo-1490806843957-31f4c9a91c65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80'
+  },
+  {
+    name: 'Токио',
+    link: 'https://images.unsplash.com/photo-1542051841857-5f90071e7989?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  }
+]; 
+
+const container = document.querySelector('.content');
+const editButton = container.querySelector('.profile__edit-button');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const editFormElement = document.querySelector('.popup__form_type_edit-profile');
+const usernameInput = editFormElement.querySelector('.popup__input_type_username');
+const aboutUserInput = editFormElement.querySelector('.popup__input_type_about-user');
+const username = container.querySelector('.profile__name');
+const aboutUser = container.querySelector('.profile__description');
+const resetButtons = document.querySelectorAll('.popup__reset-button');
+const plusButton = container.querySelector('.profile__add-button');
+const popupAddCard = document.querySelector('.popup_type_add-card');
+const cardsContainer = container.querySelector('.cards');
+const picturePopup = document.querySelector('.popup_type_picture');
+const popupImage = document.querySelector('.popup__image');
+const popupImageName = document.querySelector('.popup__image-name');
+const addCardFormElement = document.querySelector('.popup__form_type_add-card');
+const pictureNameInput = document.querySelector('.popup__input_type_picture-name');
+const pictureLinkInput = document.querySelector('.popup__input_type_picture-link');
+const popups = document.querySelectorAll('.popup');
+const validators = {};
+
+export const validationConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+};
+
+export {
+  editButton,
+  popupEditProfile,
+  editFormElement,
+  usernameInput,
+  aboutUserInput,
+  username,
+  aboutUser,
+  resetButtons,
+  plusButton,
+  popupAddCard,
+  cardsContainer,
+  picturePopup,
+  popupImage,
+  popupImageName,
+  addCardFormElement,
+  pictureNameInput,
+  pictureLinkInput,
+  popups,
+  validators
+}
