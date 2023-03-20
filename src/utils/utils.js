f0e5147ae71a51b6e576e2792a613ff617f4fd59
Rita Kruglova <kruglova404@mail.ru>
2023-03-20 12:22:46 +0300

    fix: structure of files

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,7 +12,6 @@ import {
 
 import FormValidator from '../components/FormValidator.js';
 import Card from '../components/Card.js';
-import Popup from '../components/Popup.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
